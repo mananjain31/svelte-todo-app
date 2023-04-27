7a8c913db0821d1757efca5c559d5f76e19e14af
Manan Jain <mananjain31jan@gmail.com>
2023-04-27 18:38:35 +0530

    :sparkles: Store synced with localstorage

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { writable } from "svelte/store";
 
-export const TodosStore = writable([
-  {id: 0, text: "Learn Svelte", completed : false},
-  {id: 1, text: "Learn Angular", completed : true}
-]);
+export const TodosStore = writable(JSON.parse(localStorage.getItem("todos")) || []);
+
+TodosStore.subscribe(data => localStorage.todos = JSON.stringify(data))
